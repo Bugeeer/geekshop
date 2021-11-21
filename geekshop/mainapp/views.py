f5bfdb487976053d15fb8ae9f0a30ad51772b2ec
Buger <cacashcaka@mail.ru>
2021-11-21 13:45:58 +0300

    Updated context

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'Geekshop', }
+    return render(request, 'mainapp/index.html', context)
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'title': 'Geekshop Catalog', }
+    return render(request, 'mainapp/products.html', context)
