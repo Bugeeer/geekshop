d9d78d296a58002eafee97ebf927faa07da7bf12
Buger <cacashcaka@mail.ru>
2021-11-23 13:30:23 +0300

    added media for products

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from mainapp.views import index,products
+from django.conf import settings
+from django.conf.urls.static import static
+from mainapp.views import index, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
