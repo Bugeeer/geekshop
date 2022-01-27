0320cdcfc6e971f5fcd7ca51ea796d4706512416
Buger <cacashcaka@mail.ru>
2022-01-27 10:49:43 +0300

    C2_LS_6 toolbar+ basket fix

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from mainapp.views import index, products
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +31,6 @@ urlpatterns = [
     path('orders/', include('ordersapp.urls', namespace='orders')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns +=[re_path('^__debug__/', include(debug_toolbar.urls))]
