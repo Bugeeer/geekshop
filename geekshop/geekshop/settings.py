777ce255ea328dc40306169594e8743b629bad97
Buger <cacashcaka@mail.ru>
2022-01-31 12:00:02 +0300

    LS_7

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -221,7 +221,7 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached',
         'LOCATION': '194.67.111.115:11211'
     }
 }
