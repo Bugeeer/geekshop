56e757769b2a92c7a1bec836472f69f847173d67
Buger <cacashcaka@mail.ru>
2022-01-31 14:40:19 +0300

    LS_7

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -6,16 +6,16 @@ from django.views.generic import DetailView
 
 from mainapp.models import Product, ProductCategory
 
-def get_link_category():
-    if settings.LOW_CACHE:
-        key = 'link_category'
-        link_category = cache.get(key)
-        if link_category is None:
-            link_category = ProductCategory.objects.all()
-            cache.set(key, link_category)
-        return link_category
-    else:
-        return ProductCategory.objects.all()
+# def get_link_category():
+#     if settings.LOW_CACHE:
+#         key = 'link_category'
+#         link_category = cache.get(key)
+#         if link_category is None:
+#             link_category = ProductCategory.objects.all()
+#             cache.set(key, link_category)
+#         return link_category
+#     else:
+#         return ProductCategory.objects.all()
 
 def index(request):
     context = {
@@ -46,8 +46,8 @@ def products(request, id_category=None, page=1):
         products_paginator = paginator.page(paginator.num_pages)
 
     context['products'] = products_paginator
-    # context['categories'] = ProductCategory.objects.all()
-    context['categories'] = get_link_category()
+    context['categories'] = ProductCategory.objects.all()
+    # context['categories'] = get_link_category()
     return render(request, 'mainapp/products.html', context)
 
 
